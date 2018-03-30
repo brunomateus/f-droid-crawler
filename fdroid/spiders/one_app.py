1c213c24f8f151a8080b795e092b7d638547e7b5
brunomateus <brunomateus@gmail.com>
2018-03-31 01:58:01 +0200

    Crawler that receives a input file with app links
    - Settings altered to create a friendly crawler

--- fdroid/spiders/one_app.py
+++ fdroid/spiders/one_app.py
@@ -6,7 +6,21 @@ from fdroid.items import AppItem
 class OneAppSpider(scrapy.Spider):
     name = 'one_app'
     allowed_domains = ['www.f-droid.org']
-    start_urls = ['https://f-droid.org/en/packages/de.geeksfactory.opacclient/']
+
+    def __init__(self, package_list_file=None, *args, **kwargs):
+        super(OneAppSpider, self).__init__(*args, **kwargs)
+        if package_list_file:
+            with open(package_list_file, "rt") as f:
+                self.packages = [url.strip() for url in f.readlines()]
+        else:
+            print("Packages list file not informed. Usa -a package_list_file=path")
+            exit
+
+    def start_requests(self):
+        for idx, package in enumerate(self.packages):
+            url = 'https://www.f-droid.org/en/packages/%s' % package
+            print("[ %s ] - %s" % (idx + 1, url))
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         app_name = response.css('h3.package-name::text').extract()[0].strip()
@@ -14,6 +28,7 @@ class OneAppSpider(scrapy.Spider):
         last_version_name = response.css('ul.package-versions-list > .package-version:first-child  > .package-version-header a:first-child::attr(name)').extract()
         added_on = response.css('ul.package-versions-list > .package-version:first-child  > .package-version-header::text').extract()
         last_version_code = response.css('ul.package-versions-list > .package-version:first-child  > .package-version-header a:nth-child(2)::attr(name)').extract()
+        download_url = response.css('ul.package-versions-list > .package-version:first-child  > .package-version-download a:first-child::attr(href)').extract()
 
         item = AppItem()
         item['name'] = app_name
@@ -21,5 +36,5 @@ class OneAppSpider(scrapy.Spider):
         item['version_name'] = last_version_name
         item['version_number'] = last_version_code
         item['added_on']  = added_on[len(added_on) - 1].strip().split('on')[1]
-
+        item['download_url'] = download_url
         yield item
